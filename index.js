51efda932e7c4c3dde74e36f3f70d9ebb674a553
Paul Davis <pfrankdavis@gmail.com>
2021-06-10 13:14:11 -0500

    061021.1315

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// var myIndex = 0;
-// carousel();
+let index = 0;
+carousel();
 
-// function carousel() {
-//   var i;
-//   var x = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < x.length; i++) {
-//     x[i].style.display = "none";  
-//   }
-//   myIndex++;
-//   if (myIndex > x.length) {myIndex = 1}    
-//   x[myIndex-1].style.display = "block";  
-//   setTimeout(carousel, 6000);
-// }
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  index++;
+  if (index > x.length) {index = 1}    
+  x[index-1].style.display = "block";  
+  setTimeout(carousel, 6000);
+}
 
 document.getElementById('open-modal').addEventListener('click', function() {
     document.querySelector('.modal-bg').style.display = 'flex';
